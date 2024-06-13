dd453604a6fac18c7883bc3dc75e4a50b868835c
Costo <Costo@LEROYCOSTORDELL>
2024-06-13 12:07:09 -0400

    Add project files.

---
+++ Movies8.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movies8.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
